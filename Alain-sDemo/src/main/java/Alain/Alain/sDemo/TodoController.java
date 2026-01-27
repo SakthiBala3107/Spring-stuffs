822fc4fb50508ccf76e54b9a2540b488c7af6724
Azula <Azula@gmail.com>
2026-01-27 19:01:45 +0530

    Completion of Getmapping concepts

--- Alain-sDemo/src/main/java/Alain/Alain/sDemo/TodoController.java
+++ Alain-sDemo/src/main/java/Alain/Alain/sDemo/TodoController.java
@@ -1,4 +1,30 @@
 package Alain.Alain.sDemo;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/todo")
 public class TodoController {
+    @GetMapping("/get")
+    String Todo(){
+        return "Toda";
+    }
+
+//    path variable
+    @GetMapping("/{id}")
+    String getTodoById(@PathVariable int id){
+      return String.valueOf(id);
+    }
+
+    //request param
+
+    @GetMapping
+    String getTodoByParam(@RequestParam("todoId") int id){
+        return String.valueOf(id);
+    }
+
+    @GetMapping("/create")
+    String createUser(@RequestParam("name" ) String name, @RequestParam("password") String password){
+        return "Name:" +name+"password:" + password;
+    }
 }

e1ad2685b370de985cae4abb32c401d7d8e9afad
Azula <Azula@gmail.com>
2026-01-27 14:53:57 +0530

    First commit

---
+++ Alain-sDemo/src/main/java/Alain/Alain/sDemo/fakeController.java
@@ -0,0 +1,13 @@
+package Alain.Alain.sDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class fakeController {
+@GetMapping("/hello")
+    String sayHello(){
+        return "Yo you are here in Java backedn";
+
+    }
+}

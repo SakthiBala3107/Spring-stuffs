cacaeee9278d190a0cf39d771bb5cf7f44dfab61
Azula <Azula@gmail.com>
2026-01-29 16:15:31 +0530

    Intializtion of auth jwt token , filters and all routes are protected

--- Alain-sDemo/src/main/java/Alain/Alain/sDemo/controllers/TestController.java
+++ Alain-sDemo/src/main/java/Alain/Alain/sDemo/controllers/TestController.java
@@ -1,20 +1,20 @@
-package Alain.Alain.sDemo.controllers;
-
-import Alain.Alain.sDemo.service.TestService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/test")
-public class TestController {
-
-    @Autowired
-    private TestService testService;
-
-    @GetMapping("/access/Db")
-    String accessService(){
-        return testService.accessRepository();
-    }
-}
+//package Alain.Alain.sDemo.controllers;
+//
+//import Alain.Alain.sDemo.service.TestService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/test")
+//public class TestController {
+//
+//    @Autowired
+//    private TestService testService;
+//
+//    @GetMapping("/access/Db")
+//    String accessService(){
+//        return testService.accessRepository();
+//    }
+//}
